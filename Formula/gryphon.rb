59142c8efde1be06c7c76eb890e219e5785db57f
Vinicius Vendramini <vinivendra@gmail.com>
2020-06-16 15:04:58 -0300

    Initial commit.

---
+++ Formula/gryphon.rb
@@ -0,0 +1,33 @@
+class Gryphon < Formula
+  desc "The Swift to Kotlin translator."
+  homepage "https://vinivendra.github.io/Gryphon/"
+  url "https://github.com/vinivendra/Gryphon/archive/v0.7.tar.gz"
+  sha256 "0605f925ded3756b9864dd66e43c426ba93acf0bda76f099b223926eb5b7a75f"
+
+  head "https://github.com/vinivendra/Gryphon.git", :branch => "development"
+
+  depends_on :xcode => "11.0" # Xcode 11 is the first to come with Swift 5.1
+  depends_on "ruby"
+  depends_on "kotlin" => :recommended
+
+  resource "xcodeproj" do
+    url "https://rubygems.org/downloads/xcodeproj-1.16.0.gem"
+    sha256 "7109069ba1f73f524add89c6b1812b73ba41a30d0d8ad37de206b54f8d4773ae"
+  end
+
+  def install
+    # Build the project
+    system "swift", "build"
+    # Copy the built executable to the appropriate location
+    bin.install ".build/debug/gryphon"
+  end
+
+  test do
+    # Create a swift file
+    File.write("test.swift", "print(\"Swift\") // gryphon value: println(\"Kotlin\")\n")
+
+    # Translate the swift file and check the output
+    assert_equal "println(\"Kotlin\")",
+      shell_output("#{bin}/gryphon test.swift --no-main-file").strip
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# Gryphon - Homebrew Tap
+
+This repository enables the use of [Homebrew](https://brew.sh) to install [Gryphon](https://github.com/vinivendra/Gryphon)
+
+## How do I install Gryphon?
+
+With homebrew installed, run the following:
+
+````
+$ brew install vinivendra/gryphon/gryphon
+````
+
+Alternatively, add the tap and install Gryphon separately.
+
+````
+$ brew tap vinivendra/gryphon
+$ brew install gryphon
+````
+
+## Documentation
+Run `brew help`, `man brew`, or check [Homebrew's documentation](https://docs.brew.sh).
